86c119668f6ec52029c398f8617ca3b796f3e9b4
Erin <erin@Generals-MacBook-Air.local>
2019-12-01 10:06:24 -0500

    added the provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import './index.css';
 import App from './components/App';
 
 import store from './store'
 import { createContact, deleteContact, updateContact } from './actions/contact'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 console.log(store.getState())
 store.subscribe(() => console.log(store.getState()))
