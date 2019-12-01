e592f1082f856a995c888c674bc0e48cce6ab61a
Erin <erin@Generals-MacBook-Air.local>
2019-12-01 15:37:00 -0500

    added contact list container component

---
+++ src/containers/ContactList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import List from '../components/List'
+import Item from '../components/Item'
+
+import { connect } from 'react-redux'
+import { deleteContact } from '../actions/contact'
+
+const Contacts = ({ contacts, onDelete }) => {
+    return (
+        <List>{
+            contacts.map((contact, index) => (
+                <Item
+                key={index}
+                {...contact}
+                onClick={evt => {
+                    evt.preventDefault()
+                    onDelete(index)
+                }}
+                />
+            ))}
+        </List>
+    )
+}
+
+const mapStateToProps = state => ({
+    contacts: state.contacts
+})
+
+const mapDispatchToProps = dispatch => ({
+    onDelete: id => dispatch(deleteContact(id))
+})
+
+const ContactList = connect(mapStateToProps, mapDispatchToProps)(Contacts)
+export default ContactList
