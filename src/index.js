b8d0b555d9f0cdc15c91d996dbb34cd0dbb03106
Erin <erin@Generals-MacBook-Air.local>
2019-12-02 16:46:17 -0500

    cleaned up main index file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './components/App';
 
 import store from './store'
-import { createContact, deleteContact, updateContact } from './actions/contact'
+// import { createContact, deleteContact, updateContact } from './actions/contact'
 
 ReactDOM.render(
     <Provider store={store}>
@@ -16,9 +16,3 @@ ReactDOM.render(
 
 console.log(store.getState())
 store.subscribe(() => console.log(store.getState()))
-
-store.dispatch(createContact('Erin', 'erinkelley27@gmail.com', '202-714-8726', 1))
-store.dispatch(createContact('Justin', 'justinlawrence3000@gmail.com', '859-652-2356', 2))
-// store.dispatch(deleteContact(1))
-// store.dispatch(updateContact(2, {name: 'Justy'}))
-
